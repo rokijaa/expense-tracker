66c10b2080398bec4adf5467fce8041ee5cf706e
rokija <rokijagames@gmail.com>
2024-09-24 15:51:32 +0200

    navbar responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Navigation } from './Navigation.jsx'
+import { Navbar } from './Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navigation />
+    <Navbar />
   </StrictMode>,
 )
