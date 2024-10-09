c6def89e6e8727748196670b3e808a5cdea52237
rokija <rokijagames@gmail.com>
2024-10-09 20:45:03 +0200

    pie center text

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,29 +1,49 @@
 import { ChartContainer, PieChart } from "@mui/x-charts"
-import React, {useState} from "react"
+import { useDrawingArea } from '@mui/x-charts/hooks';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
 import './content.scss'
+
+const data = [
+    { value: 30, label: 'Investments' },
+    { value: 30, label: 'Real Estate' },
+    { value: 15, label: 'Car Business' },
+    { value: 20, label: 'Plan D' },
+  ];
+  
+  const size = {
+    width: 400,
+    height: 200,
+  };
+  
+  const StyledText = styled('text')(({ theme }) => ({
+    fill: theme.palette.text.primary,
+    textAnchor: 'middle',
+    dominantBaseline: 'central',
+    fontSize: 20,
+  }));
+
+function PieCenterLabel({ children }) {
+    const { width, height, left, top } = useDrawingArea();
+    return (
+      <StyledText x={left + width / 2} y={top + height / 2}>
+        {children}
+      </StyledText>
+    );
+  }
 
 export function Content() {
     return(
         <div className="cont__user">
-            <h1 className="cont__networth">Networth</h1>
-            <PieChart className="cont__pie"
-                series={[
-                    {
-                    data: [
-                        { id: 0, value: 188035, label: "Investments"},
-                        { id: 1, value: 2000000, label: "House"},
-                        { id: 2, value: 150000, label: "Business"},
-                    ],
-                    outerRadius: 100,
-                    innerRadius: 90,
-                    label: "siblby",
-                    },
-                ]}
-                width={400}
-                height={200}
-            >
-            <div>2,338,035</div>
-            </PieChart>
+            <div className="cont__networth">
+                <h1>Networth</h1>
+                <PieChart className="cont__pie" 
+                    series={[{ data, innerRadius: 80 }]} {...size}>
+                <PieCenterLabel>19844513$</PieCenterLabel>
+                </PieChart>
+            </div>
+            <div className="cont__chart"></div>
+            <div className="cont__stats"></div>
         </div>
     )
 }
